3748614f2bfb060d3f838ab3069e56f58766aa09
Bitos88 <tyrion72@gmail.com>
2022-05-30 20:01:38 +0200

    Initial Commit

---
+++ ScoresAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoresAPP
+//
+//  Created by Alberto Alegre Bravo on 30/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
